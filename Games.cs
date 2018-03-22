91f84bb922ee253042dbbe584407dcfe058aaffc
cnola008 <32406580+cnola008@users.noreply.github.com>
2018-03-22 17:42:20 -0400

    Update Games.cs

--- Games.cs
+++ Games.cs
@@ -28,7 +28,7 @@ namespace GamesScoresLibrary
         {
             string score1 = "(" + Team1score + ")";
             string hyphen = " - ";
-            string score2 = "(" + Team2score + ")";
+            string score2 = "(" + Team2score + ")";//Creating conflict part 2 contribution
             //Step 10 modify code
             return string.Format("{0} {1} {2} {3} {4}", Team1, score1, hyphen, Team2, score2);
         }
